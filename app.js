0e0be9a2079085165f171d266234a4a4613bd843
Robert Le <robertle1120@yahoo.com>
2023-10-05 13:05:20 -0400

    added post task method

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const PORT = 3000; // Set port for server to run on
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use("/tasks", tasks);
+
+app.use((err, req, res, next) => {
+  console.errror(err.stack);
+  res.status(500).send("Error! Something went wrong!");
+});
 
 app.listen(PORT, () => {
   console.log(
--- tasks.js
+++ tasks.js
@@ -8,6 +8,12 @@ router.get("/", async (req, res) => {
   res.send(tasks);
 });
 
+router.post("/", async (req, res) => {
+  const { task, date, priority } = req.body
+  const t = await addTask(task, date, priority)
+  res.status(201).send(t);
+});
+
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   if (id == "low") {
@@ -23,11 +29,6 @@ router.get("/:id", async (req, res) => {
     const task = await getTask(id);
     res.send(task);
   }
-});
-
-router.use((err, req, res, next) => {
-  console.errror(err.stack);
-  res.status(500).send("Error! Something went wrong.");
 });
 
 export default router;
